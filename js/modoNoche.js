48251394fd4a22d1ee1c106350eb6ef94c8f16bf
bobosd <56109651+bobosd@users.noreply.github.com>
2020-01-31 14:27:24 +0100

    modo noche

---
+++ js/modoNoche.js
@@ -0,0 +1,7 @@
+function noche(){
+	var body=document.body;
+	var contenedor=document.getElementById('contenedor');
+	contenedor.style.backgroundColor='black';
+	body.style.backgroundColor='black';
+	
+}
