59053d642d3d14bc2c52145fbaec6f5c934f5d6c
bobosd <56109651+bobosd@users.noreply.github.com>
2020-02-02 14:02:16 +0100

    1

--- js/modoNoche.js
+++ js/modoNoche.js
@@ -1,7 +1,24 @@
-function noche(){
-	var body=document.body;
-	var contenedor=document.getElementById('contenedor');
-	contenedor.style.backgroundColor='black';
-	body.style.backgroundColor='black';
-	
+function noche() {
+	var body = document.body;
+	var contenedor = document.getElementById('contenedor');
+	var CTSPV = document.getElementById('CTSPV');
+	var switchNoche = document.getElementById('activarNoche')
+
+	contenedor.style.backgroundColor = 'black';
+	contenedor.style.color = '#A9A9A9';
+	body.style.backgroundColor = '#A9A9A9';
+	CTSPV.src = "imagenes/cebancNoche.png";
+	switchNoche.setAttribute=("onlick","dia();");
+}
+function dia() {
+	var body = document.body;
+	var contenedor = document.getElementById('contenedor');
+	var CTSPV = document.getElementById('CTSPV');
+	var switchNoche = document.getElementById('activarNoche')
+
+	contenedor.style.backgroundColor = 'white';
+	contenedor.style.color = 'black';
+	body.style.backgroundColor = 'white';
+	CTSPV.src = "imagenes/cebanc.png";
+	switchNoche.onClick="noche()";
 }
