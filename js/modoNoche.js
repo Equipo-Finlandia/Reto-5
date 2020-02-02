9561480fc3cebef508436e1cffb1df563337a636
bobosd <56109651+bobosd@users.noreply.github.com>
2020-02-02 20:21:05 +0100

    modo noche

--- js/modoNoche.js
+++ js/modoNoche.js
@@ -1,24 +1,43 @@
-function noche() {
-	var body = document.body;
-	var contenedor = document.getElementById('contenedor');
-	var CTSPV = document.getElementById('CTSPV');
-	var switchNoche = document.getElementById('activarNoche')
+var noche=localStorage.getItem('noche');
 
-	contenedor.style.backgroundColor = 'black';
-	contenedor.style.color = '#A9A9A9';
-	body.style.backgroundColor = '#A9A9A9';
-	CTSPV.src = "imagenes/cebancNoche.png";
-	switchNoche.setAttribute=("onlick","dia();");
+window.onload=function(){
+	if (this.noche=='1'||this.noche==null){
+		this.noche='1';
+		localStorage.setItem('noche','1');
+		console.log(this.noche);
+	}
+	if (this.noche=='0'){
+		this.noche='0';
+		localStorage.setItem('noche','0');
+		console.log(this.noche);
+	}
+	modoNoche(this.noche);
 }
-function dia() {
+
+function modoNoche(modo) {
 	var body = document.body;
 	var contenedor = document.getElementById('contenedor');
 	var CTSPV = document.getElementById('CTSPV');
-	var switchNoche = document.getElementById('activarNoche')
-
-	contenedor.style.backgroundColor = 'white';
-	contenedor.style.color = 'black';
-	body.style.backgroundColor = 'white';
-	CTSPV.src = "imagenes/cebanc.png";
-	switchNoche.onClick="noche()";
+	
+	if (this.noche=='0') {
+		contenedor.style.backgroundColor = 'black';
+		contenedor.style.color = '#A9A9A9';
+		body.style.backgroundColor = '#A9A9A9';
+		CTSPV.src = "imagenes/cebancNoche.png";
+		this.noche='1';
+	}else{
+		contenedor.style.backgroundColor='white';
+		contenedor.style.color = 'black';
+		body.style.backgroundColor = 'white';
+		CTSPV.src = "imagenes/cebanc.jpg";
+		this.noche='0';
+	}
+	console.log(this.noche);
+	console.log(modo);
+	if(modo==null){
+		if(this.noche=='0')
+			localStorage.setItem('noche','1');
+		else
+			localStorage.setItem('noche','0');
+	}
 }
